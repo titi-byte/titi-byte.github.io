78d991165b4bbf839435649bd32ea8d7616f6815
titi-byte <jarmandomontenegro.07@gmail.com>
2025-03-03 21:52:15 -0500

    Add files via upload

---
+++ flores/script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+  document.querySelector(".envelope").classList.add("open");
+  document.getElementById("message").classList.add("open");
+}
+function restartPage() {
+  location.reload();
+}
